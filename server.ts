77902b23cbab57ef1aa27c598335795415c624c1
Nikunj Beladiya <ndbeladiya720@gmail.com>
2023-07-11 00:29:26 +0530

    apply rate limit for all the apis starting with specific route

--- server.ts
+++ server.ts
@@ -14,6 +14,24 @@ const limiter = rateLimit({
 app.use(limiter)
 
 
+const apiLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+// Apply the rate limiting middleware to API calls only
+app.use('/api', apiLimiter)
+
+// Routes with /api 
+app.get('/api/user', (req, res) => {
+    res.json("Requesting for User details");
+})
+app.post('/api/user/1', (req, res) => {
+    res.json("Trying to update user");
+})
+
 
 app.listen(3000, () => {
     console.log("Server is Successfully Running, and App is listening on port "+ 3000)
